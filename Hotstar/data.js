0a8af3700b11587edaa51a85d3abe2400832e971
SHOVIK CHAKRABORTY <113230439+cshovik@users.noreply.github.com>
2023-04-22 12:42:32 +0530

    Create data.js

---
+++ Hotstar/data.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'falcon and the winter soldier',
+        des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world. Watch Raya and',
+        image: 'images/slider 2.PNG'
+    },
+    {
+        name: 'loki',
+        des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world. Watch Raya and',
+        image: 'images/slider 1.PNG'
+    },
+    {
+        name: 'wanda vision',
+        des: 'Wanda Maximoff dan Vision, dua makhluk super yang menjalani kehidupan pinggiran kota yang ideal, mulai curiga semuanya tidak seperti yang ...',
+        image: 'images/slider 3.PNG'
+    },
+    {
+        name: 'raya and the last dragon',
+        des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world. Watch Raya and',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'luca',
+        des: 'Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world. Watch Raya and',
+        image: 'images/slider 5.PNG'
+    }
+]
